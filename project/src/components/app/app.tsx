b0c34aee8b397dd618ca4fc370a663af42e1d080
sergiivelikorodnov <sergii.velikorodnov@gmail.com>
2021-10-08 10:59:54 +0200

    feat: add Router

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import Main from '../main/main';
+import Favorites from '../favorites/favorites';
+import NotFound from '../not-found/not-found';
+import Login from '../login/login';
+import Property from '../property/property';
+
 
 const Settings = {
   PROPERTY_NUMBER: 322,
@@ -6,9 +13,29 @@ const Settings = {
 
 function App(): JSX.Element {
   return (
-    <Main
-      propertyNumber = {Settings.PROPERTY_NUMBER}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Main}>
+          <Main
+            propertyNumber={Settings.PROPERTY_NUMBER}
+          />
+        </Route>
+        <Route exact path={AppRoute.Login}>
+          <Login />
+        </Route>
+        <Route exact path={AppRoute.Favorites}>
+          <Favorites />
+        </Route>
+        <Route exact path={AppRoute.Property}>
+          <Property
+            price={122}
+          />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/components/not-found/not-found.tsx
@@ -0,0 +1,49 @@
+function NotFound(): JSX.Element{
+  return (
+    <div className="page">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link" href="main.html">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+              </a>
+            </div>
+            <nav className="header__nav">
+              <ul className="header__nav-list">
+                <li className="header__nav-item user">
+                  <a className="header__nav-link header__nav-link--profile" href="#">
+                    <div className="header__avatar-wrapper user__avatar-wrapper">
+                    </div>
+                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                  </a>
+                </li>
+                <li className="header__nav-item">
+                  <a className="header__nav-link" href="#">
+                    <span className="header__signout">Sign out</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+
+      <main className="page__main page__main--favorites">
+        <div className="page__favorites-container container">
+          <section className="favorites">
+            <h1 className="favorites__title">Sorry, we could not find that page</h1>
+            <h2>Try to go  <a className="header__nav-link" href="#">Home Page</a></h2>
+          </section>
+        </div>
+      </main>
+      <footer className="footer container">
+        <a className="footer__logo-link" href="main.html">
+          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default NotFound;
