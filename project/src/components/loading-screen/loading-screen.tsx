6f10ec9cc792ae1e883e1d624f78626baa86884c
sergiivelikorodnov <sergii.velikorodnov@gmail.com>
2021-10-30 15:52:04 +0200

    feat: add custom loader

--- project/src/components/loading-screen/loading-screen.tsx
+++ project/src/components/loading-screen/loading-screen.tsx
@@ -1,6 +1,16 @@
+import Loader from 'react-loader-spinner';
+
 function LoadingScreen(): JSX.Element {
   return (
-    <p>Loading ...</p>
+    <div className="loader__wrapper">
+      <Loader
+        type="TailSpin"
+        color="#4481c3"
+        height={50}
+        width={50}
+      />
+    </div>
+
   );
 }
 
