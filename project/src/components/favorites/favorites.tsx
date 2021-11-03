c699821c8fd24b206c07eb3dd8e73b1ce9cbb13b
sergiivelikorodnov <sergii.velikorodnov@gmail.com>
2021-11-03 17:21:59 +0100

    add: empty page for favorites

--- project/src/components/favorites/favorites.tsx
+++ project/src/components/favorites/favorites.tsx
@@ -44,6 +44,38 @@ function Favorites({ favoritesOffers, loadOffersData, isDataLoaded=false }: Prop
     (offer: Offer) => offer.city.name,
   );
 
+  if (favoritesOffers.length === 0) {
+    return (
+      <div className="page">
+        <Header/>
+        <main className="page__main page__main--favorites page__main--favorites-empty">
+          <div className="page__favorites-container container">
+            <section className="favorites favorites--empty">
+              <h1 className="visually-hidden">Favorites (empty)</h1>
+              <div className="favorites__status-wrapper">
+                <b className="favorites__status">Nothing yet saved.</b>
+                <p className="favorites__status-description">
+                Save properties to narrow down search or plan your future trips.
+                </p>
+              </div>
+            </section>
+          </div>
+        </main>
+        <footer className="footer container">
+          <Link className="footer__logo-link" to={AppRoute.Main}>
+            <img
+              className="footer__logo"
+              src="img/logo.svg"
+              alt="6 cities logo"
+              width="64"
+              height="33"
+            />
+          </Link>
+        </footer>
+      </div>
+    );
+  }
+
   return (
     <div className="page">
       <Header/>
