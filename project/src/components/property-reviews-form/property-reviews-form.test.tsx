117570e47ee8086bddf520dfc63d4a3977bbeea7
sergiivelikorodnov <sergii.velikorodnov@gmail.com>
2021-11-17 12:13:15 +0100

    some fix: property reviews form

--- project/src/components/property-reviews-form/property-reviews-form.test.tsx
+++ project/src/components/property-reviews-form/property-reviews-form.test.tsx
@@ -4,7 +4,7 @@ import { createMemoryHistory } from 'history';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import thunk, { ThunkDispatch } from 'redux-thunk';
-import { fakeStateNoAuth } from '../../mocks/mock-store';
+import { fakeStateAuth } from '../../mocks/mock-store';
 import { createApi } from '../../services/api';
 import { State } from '../../types/state';
 import PropertyReviewsForm from './property-reviews-form';
@@ -22,7 +22,7 @@ const mockStore = configureMockStore <
     ThunkDispatch< State, typeof api, Action >
   >(middlewares);
 
-const store = mockStore(fakeStateNoAuth);
+const store = mockStore(fakeStateAuth);
 describe('check property reviews form', () => {
 
   it('should show form correctly', () => {
